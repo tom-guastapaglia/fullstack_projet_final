6a0c9136e3d9c9833fe3903f48bae0949377030e
FERRALI-Mario <you@example.com>
2023-01-12 18:54:09 +0100

    :sparkles: endpoints

--- proxy/src/userFunctions.ts
+++ proxy/src/userFunctions.ts
@@ -8,72 +8,94 @@ import {
 } from "./types";
 
 export const connection = (username: string, password: string) => {
-    return new Promise((resolve) => {
-        axios
-            .post(
-                endPointServiceUserLogin,
-                { username, password },
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            )
-            .then((response) => {
-                resolve(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    });
+   return new Promise((resolve) => {
+       axios
+           .post(
+               endPointServiceUserLogin,
+               { username, password },
+               {
+                   headers: {
+                       "Content-Type": "application/json",
+                   },
+               }
+           )
+           .then((response) => {
+               resolve(response.data);
+           })
+           .catch((error) => {
+               console.log(error);
+           });
+   });
 };
 
-export const inscription = (email: string, fisrtName: string, lastName: string, phone: string, country: string) => {
-    return new Promise((resolve) => {
-        axios
-            .post(
-                endPointServiceUserInscription,
-                {email, fisrtName, lastName, phone, country},
-            )
-            .then((response) => {
-                resolve(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    })
-}
-
 export const checkUserInfo = (token: string) => {
-    return new Promise((resolve) => {
-        axios
-            .get(endPointServiceUserInfo, {
-                headers: {
-                    Authorization: token,
-                },
-            })
-            .then((response) => resolve(response.data));
-    });
+   return new Promise((resolve) => {
+       axios
+           .get(endPointServiceUserInfo, {
+               headers: {
+                   Authorization: token,
+               },
+           })
+           .then((response) => resolve(response.data));
+   });
 };
 
 export const checkRole = (role: string, token: string) => {
-    return new Promise((resolve) => {
-        axios
-            .post(
-                endPointServiceUserCheckRole,
-                { role },
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: token,
-                    },
-                }
-            )
-            .then((response) => {
-                resolve(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    });
+   return new Promise((resolve) => {
+       axios
+           .post(
+               endPointServiceUserCheckRole,
+               { role },
+               {
+                   headers: {
+                       "Content-Type": "application/json",
+                       Authorization: token,
+                   },
+               }
+           )
+           .then((response) => {
+               resolve(response.data);
+           })
+           .catch((error) => {
+               console.log(error);
+           });
+   });
 };
+
+export const inscription = (email: string, fisrtName: string, lastName: string, phone: string, country: string) => {
+   return new Promise((resolve) => {
+       axios
+           .post(
+               endPointServiceUserInscription,
+               { email, fisrtName, lastName, phone, country },
+           )
+           .then((response) => {
+               resolve(response.data);
+           })
+           .catch((error) => {
+               console.log(error);
+           });
+   })
+}
+
+export const validation = (id, token) => {
+   return new Promise((resolve) => {
+      axios
+         .post(
+            endPointServiceUsersInfo,
+            { id },
+            {
+               headers: {
+                   "Content-Type": "application/json",
+                   Authorization: token,
+               },
+           }
+         )
+         .then((response) => {
+            resolve(response.data);
+         })
+         .catch((error) => {
+            console.log(error);
+         });
+   })
+}
