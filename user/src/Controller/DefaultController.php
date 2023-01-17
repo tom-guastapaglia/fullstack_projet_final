ab6b4f7976f4a10ea31ca4a392c95284eecf9485
Tom <tomguasta@yahoo.fr>
2023-01-17 13:10:11 +0100

    Login + get future users Endpoints

--- user/src/Controller/DefaultController.php
+++ user/src/Controller/DefaultController.php
@@ -5,11 +5,15 @@ namespace App\Controller;
 use App\Entity\FutureUser;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 #[Route("/")]
