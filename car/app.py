b9c2891038ac05ec7551a88cfc40c4c254e10491
Tom <tomguasta@yahoo.fr>
2023-01-20 14:58:11 +0100

    add car

--- car/app.py
+++ car/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 import json
+import models as models
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_crud.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+app.run(host='car', port=5001)
+
 
 @app.route("/", methods=["GET"])
 def hello():
