e240b131e37c2f5d23aa83dda5e60b57ded44b85
Tom Guastapaglia <tomguasta@yahoo.fr>
2023-02-10 14:50:44 +0100

    Login admin button

--- vitrine/components/HeaderComponent.tsx
+++ vitrine/components/HeaderComponent.tsx
@@ -1,12 +1,21 @@
 import { InputTextComponent, ButtonComponent } from "my-lib-ui";
+import {router} from "next/client";
+import {useRouter} from "next/router";
 
 
 
 const HeaderComponent: React.FC = () => {
-  return (
+    const router = useRouter();
+
+    //redirect to login onclick
+    const login = () => {
+        router.push("/login");
+    }
+
+    return (
     <header>
       <img src="/logo.svg" alt="" />
-      <ButtonComponent label="Connexion admin" />
+      <ButtonComponent label="Connexion admin" onClick={login} />
     </header>
   );
 };
