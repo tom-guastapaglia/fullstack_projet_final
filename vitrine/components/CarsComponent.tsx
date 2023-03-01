3e4d4d9c07c23a1c43aa179e580b22ae39484b4e
FERRALI Mario <62051103+FERRALI-Mario@users.noreply.github.com>
2023-03-01 10:14:37 +0100

    Update CarsComponent.tsx

--- vitrine/components/CarsComponent.tsx
+++ vitrine/components/CarsComponent.tsx
@@ -1,10 +1,53 @@
 import React from "react";
 
+const datas = [
+    {
+        'Marque' : 'BMW',
+        'Modele' : 'X1',
+        'Action' : 'Modifier'
+    },
+    {
+        'Marque' : 'BMW',
+        'Modele' : 'X2',
+        'Action' : 'Modifier'
+    },
+    {
+        'Marque' : 'BMW',
+        'Modele' : 'X3',
+        'Action' : 'Modifier'
+    },
+    {
+        'Marque' : 'BMW',
+        'Modele' : 'X4',
+        'Action' : 'Modifier'
+    },
+    {
+        'Marque' : 'BMW',
+        'Modele' : 'X5',
+        'Action' : 'Modifier'
+    },
+]
+
 const CarsComponent = () => {
     return (
-        <div>
-            <p>Cars</p>
-        </div>
+        <table className={"table-car"}>
+            <thead>
+                <tr>
+                    <th>Marque</th>
+                    <th>Modèle</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {datas.map((data, index) => (
+                    <tr className={"car-line"}>
+                        <th >{data.Statut}</th>
+                        <th>{data.Modele}</th>
+                        <th>{data.Action}</th>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     );
 };
 
